355e6d6d89f4143234ad0c54a21c55e5042aa1b0
Steven Blythe <blythesr@googlemail.com>
2021-09-01 19:53:52 +0100

    Initial Commit

---
+++ Stanford_EmojiArt/Stanford_EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stanford_EmojiArtApp.swift
+//  Stanford_EmojiArt
+//
+//  Created by Steve Blythe on 01/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Stanford_EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
